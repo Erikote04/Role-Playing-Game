21223fcd1e3582d32bfb98c504047e74453a1633
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-17 10:45:58 +0100

    Step 59

--- script.js
+++ script.js
@@ -35,6 +35,20 @@ const locations = [
             fightDragon
         ],
         text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": [
+            "Buy 10 health (10 gold)", 
+            "Buy weapon (30 gold)",
+            "Go to town square"
+        ],
+        "button functions": [
+            buyHealth,
+            buyWeapon,
+            goTown
+        ],
+        text: "You enter the store"
     }
 ];
 
