007eaa5640e60eb66dc525e4c9fc2f04d9d8d187
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-17 10:53:04 +0100

    Step 65

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-    button1.innerText = location["button text"];
+    button1.innerText = location["button text"][0];
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
 
