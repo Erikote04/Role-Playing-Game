a027c84d8da7012c22ae2844bdcb4714454be9f8
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-18 10:21:46 +0100

    Step 133

--- script.js
+++ script.js
@@ -115,6 +115,20 @@ const locations = [
         ],
         text: "You are fighting a monster."
     },
+    {
+        name: "kill monster",
+        "button text": [
+            "Go to town square", 
+            "Go to town square",
+            "Go to town square"
+        ],
+        "button functions": [
+            goTown,
+            goTown,
+            goTown
+        ],
+        text: "The monster screams Arg! as it dies. You gain experience points and find gold."
+    },
 ];
 
 // initialize buttons
