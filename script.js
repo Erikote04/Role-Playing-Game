6f97ffd84957f6e9d60205ee9b41235ca1bb0874
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-18 21:29:46 +0100

    Step 172

--- script.js
+++ script.js
@@ -349,6 +349,9 @@ function pick(guess) {
     for (let i = 0; i < numbers.length; i++) {
         text.innerText += numbers[i] + "\n";
     }
+    if (numbers.includes(guess)) {
+    
+    }
 }
 
 function pickTwo() {
