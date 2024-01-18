6ae9b4c75ddfdf82d482c136f56ebaf991e0ace1
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-18 10:11:51 +0100

    Step 126

--- script.js
+++ script.js
@@ -213,15 +213,17 @@ function goFight() {
 }
 
 function attack() {
-    if (health <= 0) {
-        lose();
-    }
     text.innerText = "The " + monsters[fighting].name + " attacks";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name;
     health -= monsters[fighting].level;
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
+    }
 }
 
 function dodge() {
